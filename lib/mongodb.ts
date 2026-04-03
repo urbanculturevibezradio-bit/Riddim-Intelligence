1f61602a9fcfac64b1dd4c9b872279c1864ff374
benny <urbanculturevibezradio@gmail.com>
2026-04-03 17:33:59 -0400

    Fix mongodb URI build error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,10 +2,6 @@ import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI as string;
 
-if (!uri) {
-  throw new Error('Please add your MongoDB URI to .env.local');
-}
-
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
